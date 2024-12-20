7540819e513838be2f871efad85b7d2e04be5671
royaboya <94084201+royaboya@users.noreply.github.com>
2024-12-20 01:29:38 -0500

    trying to fix routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://royaboya.github.io',
-	base: 'royaboyaa',
+	site: 'https://royaboyaz.github.io',
 	integrations: [mdx(), sitemap()],
 });
